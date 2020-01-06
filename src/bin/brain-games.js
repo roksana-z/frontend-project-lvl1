8614beb5f351f20a4a0eb50348560b7dd00eff4f
Boudi <boudi@inbox.ru>
2020-01-06 17:48:15 +0300

    changed launch logic

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import { question } from '..';
 
-console.log('Welcome to the Brain Games!');
 
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
+question();
