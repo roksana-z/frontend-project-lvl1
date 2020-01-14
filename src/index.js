85a2c0c8294d93832665a26da9a0e0742d80d063
Boudi <boudi@inbox.ru>
2020-01-14 17:17:30 +0300

    game-calc

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable consistent-return */
 import readlineSync from 'readline-sync';
+import { cons, car, cdr } from '@hexlet/pairs';
 
-export const greeting = () => {
-  console.log('Welcome to the Brain Games!\nAnswer "yes" if the number is even, otherwise answer "no".');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  const saveName = () => name;
+export const greeting = (game) => {
+  switch (game) {
+    case 'even':
+      console.log('Welcome to the Brain Games!\nAnswer "yes" if the number is even, otherwise answer "no".');
+      break;
+    case 'calc':
+      console.log('Welcome to the Brain Games!\nWhat is the result of the expression?');
+      break;
+    default: break;
+  }
+};
+
+export const name = () => {
+  const name2 = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name2}!`);
+  const saveName = () => name2;
   return saveName;
 };
 
@@ -14,26 +28,67 @@ export const getRandomIntInclusive = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-export const isEven = (arg) => arg % 2 === 0;
+export const number = () => getRandomIntInclusive(0, 10);
 
-export const checkAnswer = (arg, bool) => {
-  if (bool === true && arg === 'yes' || bool === false && arg === 'no') {
-    return 'correct';
-  } return 'not correct';
-};
+export const isEven = (arg) => (arg % 2 === 0 ? 'yes' : 'no');
 
 export const question = () => {
-  const getName = greeting();
+  greeting('even');
+  const getName = name();
   let counter = 0;
   while (counter < 3) {
-    const number = getRandomIntInclusive(0, 1000);
-    console.log(`Question: ${number}`);
+    const getNumber = getRandomIntInclusive(0, 10);
+    console.log(`Question: ${getNumber}`);
+    const actualAnswer = isEven(getNumber);
     const answer = readlineSync.question('Your answer:');
-    if (checkAnswer(answer, isEven(number)) === 'correct') {
-      console.log('Correct!');
+    if (answer === actualAnswer) {
+      console.log('correct!');
       counter += 1;
     } else {
-      console.log(`'yes' is wrong answer ;(.\nCorrect answer was 'no'. Let's try again, ${getName()}`);
+      console.log(`'${answer}' is wrong answer ;(.\nCorrect answer was '${actualAnswer}'. Let's try again, ${getName}`);
     }
-  }console.log(`Congratulations, ${getName()}!`);
+  }
+  console.log(`Congratulations, ${getName()}!`);
+};
+
+export const pair1 = cons(number(), number());
+export const pair2 = cons(number(), number());
+export const pair3 = cons(number(), number());
+
+export const stringSum = `${car(pair1).toString()} + ${cdr(pair1).toString()}`;
+
+export const stringMult = `${car(pair2).toString()} * ${cdr(pair2).toString()}`;
+
+
+export const stringSubtr = `${car(pair3).toString()} - ${cdr(pair3).toString()}`;
+
+
+export const calcSum = car(pair1) + cdr(pair1);
+export const calcMult = car(pair2) * cdr(pair2);
+export const calcSubtr = car(pair3) - cdr(pair3);
+
+export const questionCalc = () => {
+  greeting('calc');
+  const getName = name();
+  const listOfCalcStrings = cons(stringSum, cons(stringSubtr, cons(stringMult, null)));
+  const listOfCalcs = cons(calcSum, cons(calcSubtr, cons(calcMult, null)));
+  const iter = (list1, list2) => {
+    if (list1 === null) {
+      console.log(`Congratulations, ${getName()}`);
+      return;
+    }
+    console.log(`Question:${car(list1)}`);
+    const actualAnswer = car(list2);
+    const userAnswer = readlineSync.question('Your answer:');
+    // eslint-disable-next-line eqeqeq
+    if (userAnswer == actualAnswer) {
+      console.log('correct');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${actualAnswer}'.
+      Let's try again, ${getName()}!`);
+      return;
+    }
+    return iter(cdr(list1), cdr(list2));
+  };
+  return iter(listOfCalcStrings, listOfCalcs);
 };
