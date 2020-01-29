1011df9823ea8d0fd2d4cac90193dfc71511a53a
Boudi <boudi@inbox.ru>
2020-01-29 11:40:53 +0300

    refactoring

--- src/bin/brain-prime.js
+++ src/bin/brain-prime.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-// eslint-disable-next-line import/extensions
-import { isPrimeQuestion } from '../games/isPrime.js';
+import isPrimeQuestion from '../games/isPrime';
 
 isPrimeQuestion();
