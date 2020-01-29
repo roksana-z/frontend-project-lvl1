16753562bbe221cc93352561b92eafd9eda63d5c
Boudi <boudi@inbox.ru>
2020-01-29 11:43:12 +0300

    refactoring

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,63 +1,59 @@
-/* eslint-disable no-plusplus */
-/* eslint-disable space-infix-ops */
-/* eslint-disable use-isnan */
-import readlineSync from 'readline-sync';
-import {
-  greeting, getName, randomNumber, answer, getRandomIntInclusive,
-} from '..';
 
-export const createRandomProgression = (progressionAmount) => {
+import { randomNumber, getRandomIntInclusive, question } from '..';
+
+const createRandomProgression = (progressionAmount) => {
   const start = randomNumber();
   const array = [start];
   const step = randomNumber();
   let counter = start;
   while (array.length < progressionAmount) {
-    counter +=step;
+    counter += step;
     array.push(counter);
   }
   return array;
 };
 
-export const hideDigit = (arr) => {
+const hideDigit = (arr) => {
   const hiddenI = getRandomIntInclusive(0, arr.length - 1);
   const newArr = [];
-  for (let index = 0; index <= arr.length - 1; index++) {
+  for (let index = 0; index <= arr.length - 1; index += 1) {
     if (index === hiddenI) {
       newArr.push('..');
-      index++;
+      index += 1;
     }
     newArr.push(arr[index]);
   }
   return newArr.join(' ');
 };
 
+const description = 'What number is missing in the progression?';
 
-// eslint-disable-next-line consistent-return
-const findAnswerOfProgression = (arr1, arr2) => {
-  for (let i = 0; i <= arr1.length - 1; i++) {
+const originalProgressions = [createRandomProgression(10), createRandomProgression(10), createRandomProgression(10)];
+
+const copyProgressions = [...originalProgressions];
+
+const modifiedProgressions = copyProgressions.map(hideDigit);
+
+const findNumber = (arr1, arr2) => {
+  for (let i = 0; i < arr2.length; i += 1) {
     if (!arr2.includes(arr1[i])) {
       return arr1[i];
     }
   }
+  return undefined;
 };
 
-export const progressionQuestion = () => {
-  greeting('progression');
-  const userName = getName();
-  let counter = 0;
-  while (counter < 3) {
-    const originalpProgression = createRandomProgression(10);
-    const modifiedProgression = hideDigit(originalpProgression);
-    console.log(`Question: ${modifiedProgression}`);
-    const actualAnswer = findAnswerOfProgression(originalpProgression, modifiedProgression);
-    const userAnswer = readlineSync.question('Your answer:');
-    if (Number(userAnswer) === actualAnswer) {
-      console.log(answer(userName(), userAnswer, actualAnswer, true));
-      counter++;
-    } else {
-      console.log(answer(userName(), userAnswer, actualAnswer, false));
-      return;
-    }
+const answers = (arr1, arr2) => {
+  const result = [];
+  for (let i = 0; i < arr2.length; i += 1) {
+    const hiddenNumber = findNumber(arr1[i], arr2[i]);
+    result.push(hiddenNumber);
   }
-  console.log(`Congratulations, ${userName()}!`);
+  return result;
 };
+
+const progressionQuestion = () => {
+  question(modifiedProgressions, answers(originalProgressions, modifiedProgressions), description);
+};
+
+export default progressionQuestion;
