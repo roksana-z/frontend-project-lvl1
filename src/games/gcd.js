1c6f518492d14743405bbf8b0a0629199f227c6e
Boudi <boudi@inbox.ru>
2020-01-29 11:42:31 +0300

    refactoring

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,44 +1,36 @@
-/* eslint-disable no-plusplus */
-import readlineSync from 'readline-sync';
-import {
-  greeting, getName, randomNumber, answer,
-} from '..';
+
+import { randomNumber, question } from '..';
 
 
-export const findGcd = (num1, num2) => {
-  const iter = (num) => {
-    if (num1 % num === 0 && num2 % num === 0) {
-      return num;
-    }
-    return iter(num - 1);
-  };
-  if (num1 % num2 === 0) {
-    return num2;
-  } if (num2 % num1 === 0) {
-    return num1;
-  } if (num1 > num2) {
-    return iter(num2);
+const findGcd = (num1, num2) => (num2 === 0 ? num1 : findGcd(num1 % num2));
+
+const operands = [[randomNumber(), randomNumber()], 
+[randomNumber(), randomNumber()], [randomNumber(), randomNumber()]];
+
+const newOperands = [...operands];
+
+const separate = (arr) => {
+  const result = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    const pair = arr[i].join(' ');
+    result.push(pair);
   }
-  return iter(num1);
+  return result;
 };
 
-export const gcdQuestion = () => {
-  greeting('gcd');
-  const userName = getName();
-  let counter = 0;
-  while (counter < 3) {
-    const numberOne = randomNumber();
-    const numberTwo = randomNumber();
-    console.log(`Question: ${numberOne} ${numberTwo}`);
-    const actualAnswer = findGcd(numberOne, numberTwo);
-    const userAnswer = readlineSync.question('Your answer:');
-    if (Number(userAnswer) === actualAnswer) {
-      console.log(answer(userName(), userAnswer, actualAnswer, true));
-      counter++;
-    } else {
-      console.log(answer(userName(), userAnswer, actualAnswer, false));
-      return;
-    }
+const description = 'Find the greatest common divisor of given numbers.';
+
+const answers = (arr) => {
+  const result = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    const ans = findGcd(...arr[i]);
+    result.push(ans);
   }
-  console.log(`Congratulations, ${userName()}!`);
+  return result;
 };
+
+const gcdQuestion = () => {
+  question(separate(newOperands), answers(operands), description);
+};
+
+export default gcdQuestion;
