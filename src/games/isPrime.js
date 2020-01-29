e9250bca2170dc9147d60e8a889f63e4cf90fe37
Boudi <boudi@inbox.ru>
2020-01-29 11:42:50 +0300

    refactoring

--- src/games/isPrime.js
+++ src/games/isPrime.js
@@ -1,14 +1,11 @@
-/* eslint-disable no-plusplus */
-import readlineSync from 'readline-sync';
-import {
-  greeting, getName, randomNumber, answer,
-} from '..';
 
-export const isPrime = (num) => {
+import { question, randomNumber } from '..';
+
+const isPrime = (num) => {
   if (num <= 1) {
     return 'no';
   }
-  for (let i = 2; i < 7; i++) {
+  for (let i = 2; i < 7; i += 1) {
     if (num % i === 0 && num !== i) {
       return 'yes';
     }
@@ -16,22 +13,14 @@ export const isPrime = (num) => {
   return 'no';
 };
 
-export const isPrimeQuestion = () => {
-  greeting('prime');
-  const userName = getName();
-  let counter = 0;
-  while (counter < 3) {
-    const getNumber = randomNumber();
-    console.log(`Question: ${getNumber}`);
-    const actualAnswer = isPrime(getNumber);
-    const userAnswer = readlineSync.question('Your answer:');
-    if (userAnswer === actualAnswer) {
-      console.log(answer(userName(), userAnswer, actualAnswer, true));
-      counter++;
-    } else {
-      console.log(answer(userName(), userAnswer, actualAnswer, false));
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName()}!`);
+const numbers = [randomNumber(), randomNumber(), randomNumber()];
+
+const answers = numbers.map(isPrime);
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+const isPrimeQuestion = () => {
+  question(numbers, answers, description);
 };
+
+export default isPrimeQuestion;
