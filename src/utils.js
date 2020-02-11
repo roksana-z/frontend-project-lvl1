43f20a17859021fc6006a9276b000bb3e7b8a9e2
Boudi <boudi@inbox.ru>
2020-02-11 21:30:24 +0300

    refactoring

---
+++ src/utils.js
@@ -0,0 +1,7 @@
+const getRandomIntInclusive = (min, max) => {
+  const minNum = Math.ceil(min);
+  const maxNum = Math.floor(max);
+  return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+};
+
+export default getRandomIntInclusive;
