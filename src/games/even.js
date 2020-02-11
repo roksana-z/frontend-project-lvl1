859330390c126ad96b8ee054dc9e657df6482a39
Boudi <boudi@inbox.ru>
2020-02-11 21:26:39 +0300

    refactoring

--- src/games/even.js
+++ src/games/even.js
@@ -1,16 +1,24 @@
 
-import { randomNumber, question } from '..';
+import { question } from '..';
+import getRandomIntInclusive from '../utils';
 
-const isEven = (arg) => (arg % 2 === 0 ? 'yes' : 'no');
-
-const numbers = [randomNumber(), randomNumber(), randomNumber()];
+const isEven = (arg) => (arg % 2 === 0);
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const corectAnswers = numbers.map(isEven);
+const getResult = (num) => {
+  const result = isEven(num) ? 'yes' : 'no';
+  return result;
+};
+
+const data = () => {
+  const number = getRandomIntInclusive(0, 25);
+  const answer = getResult(number);
+  return [number, answer];
+};
 
 const evenQuestion = () => {
-  question(numbers, corectAnswers, description);
+  question(data, description);
 };
 
 export default evenQuestion;
